1e04dd339097184fe59a69bd00c669471f38f547
Phatnguyen <trangnguyen.091016@gmail.com>
2026-05-09 10:06:45 +0700

    lop executive fix

--- HelloWorld/src/communityuni/com/executive.java
+++ HelloWorld/src/communityuni/com/executive.java
@@ -1,5 +1,24 @@
 package communityuni.com;
 
-public class executive {
+public class executive extends employee{
+	private double bonus;
+	
+	public executive(String name, String address, String phone,String ssn, double rate) {
+		super(name, address, phone, ssn ,rate);
+		
+		bonus = 0;
+	}
+	
+	public void awardBonus(double execBonus) {
+		this.bonus = execBonus;
+	}
+	
+	@Override
+	public double pay() {
+		double totalPay	= super.pay() + bonus;
+		
+		bonus = 0;
+		return totalPay;
+	}
 
 }
