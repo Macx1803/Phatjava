a741336b9aad615ef28176dd13e038b435bf6bd1
Phatnguyen <trangnguyen.091016@gmail.com>
2026-05-09 09:21:51 +0700

    lop StaffMember fix

--- HelloWorld/src/communityuni/com/staffmember.java
+++ HelloWorld/src/communityuni/com/staffmember.java
@@ -1,5 +1,18 @@
 package communityuni.com;
 
-public class staffmember {
-
+public abstract class staffmember {
+	protected String name;
+	protected String address;
+	protected String phone;
+	
+	public staffmember(String name, String address, String phone) {
+		this.name=name;
+		this.address=address;
+		this.phone=phone;
+	}
+	
+	public String toString() {
+		return("Name:" + name + "\nAddress: " + address+ "\nPhone: " + phone);
+	}
+	public abstract double pay();
 }
